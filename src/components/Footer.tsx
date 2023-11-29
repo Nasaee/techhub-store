4d02e180788b18d147557f8ea40f7e493db9639e
Nasa-ee Madadam <122335609+Nasaee@users.noreply.github.com>
2023-11-30 00:11:42 +0700

    create footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,32 +1,59 @@
+import FooterCard from './footerCard';
+import { Link } from 'react-router-dom';
+import storeIcon from '../assets/logo.webp';
 const Footer = () => {
   return (
-    <footer className='bg-neutral'>
-      <div>
-        <div>free shippintg</div>
-        <div>3 hour dalivery</div>
-        <div>click & collect</div>
-        <div>easy return/refunds</div>
+    <footer className='bg-neutral p-10 pt-24'>
+      <div className='grid md:grid-cols-2 lg:grid-cols-4  gap-y-6 justify-center pb-12 border-b border-[#868e96]'>
+        <FooterCard
+          icon='truck'
+          header='Free shipping'
+          description='with minimum over 3,000.-'
+        />
+        <FooterCard
+          icon='time'
+          header='3 hour dalivery'
+          description='Bangkok and Servie areas'
+        />
+        <FooterCard
+          icon='store'
+          header='Click & Collect'
+          description='380 Branches Nationwide'
+        />
+        <FooterCard
+          icon='return'
+          header='Easy Return/Refunds'
+          description='7 day Return*'
+        />
       </div>
-      <div className='footer p-10 text-neutral-content'>
-        <nav>
-          <header className='footer-title'>Services</header>
-          <a className='link link-hover'>Branding</a>
-          <a className='link link-hover'>Design</a>
-          <a className='link link-hover'>Marketing</a>
-          <a className='link link-hover'>Advertisement</a>
-        </nav>
-        <nav>
+
+      <div className='grid md:grid-cols-2 justify-center lg:flex lg:justify-start lg:gap-[6rem] text-neutral-content mt-8'>
+        <Link to='/' className='hidden lg:block w-48'>
+          <img src={storeIcon} alt='store logo' />
+        </Link>
+        <nav className='flex flex-col gap-4 mb-10 '>
           <header className='footer-title'>Company</header>
-          <a className='link link-hover'>About us</a>
+          <Link to='/about-us' className='link link-hover'>
+            About us
+          </Link>
           <a className='link link-hover'>Contact</a>
-          <a className='link link-hover'>Jobs</a>
-          <a className='link link-hover'>Press kit</a>
+          <span>
+            <span>Phone : </span>
+            <a href='tel:+66 02 123 4567' className='link link-hover'>
+              (+66) 02 123 4567
+            </a>
+          </span>
+          <span>
+            <span>Email : </span>
+            <a className='link link-hover'>techHub_official@mail.com</a>
+          </span>
         </nav>
-        <nav>
+        <nav className='flex flex-col gap-4 mb-10 max-w-md'>
           <header className='footer-title'>Legal</header>
-          <a className='link link-hover'>Terms of use</a>
-          <a className='link link-hover'>Privacy policy</a>
-          <a className='link link-hover'>Cookie policy</a>
+          <a className='link link-hover'>Terms and Conditions</a>
+          <a className='link link-hover'>Return policy</a>
+          <a className='link link-hover'>Refunds policy</a>
+          <a className='link link-hover'>Waranty policy / Claim Notification</a>
         </nav>
       </div>
     </footer>
---
+++ src/components/footerCard.tsx
@@ -0,0 +1,33 @@
+import { LiaShippingFastSolid } from 'react-icons/lia';
+import { BsClockHistory } from 'react-icons/bs';
+import { PiBuildingsLight } from 'react-icons/pi';
+import { GiReturnArrow } from 'react-icons/gi';
+import { FC } from 'react';
+
+const FooterIcon = {
+  truck: <LiaShippingFastSolid className='w-16 h-16' />,
+  time: <BsClockHistory className='w-16 h-16' />,
+  store: <PiBuildingsLight className='w-16 h-16' />,
+  return: <GiReturnArrow className='w-16 h-16' />,
+} as const;
+
+type footerCardProps = {
+  icon: keyof typeof FooterIcon;
+  header: string;
+  description: string;
+};
+
+const FooterCard: FC<footerCardProps> = ({ icon, header, description }) => {
+  return (
+    <div className='flex gap-5 px-10 md:border-x border-[#868e96] text-neutral-content'>
+      {FooterIcon[icon]}
+      <div className='my-auto'>
+        <header className='text-base font-bold uppercase tracking-widest'>
+          {header}
+        </header>
+        <p className='text-[#868e96] tracking-wider'>{description}</p>
+      </div>
+    </div>
+  );
+};
+export default FooterCard;
