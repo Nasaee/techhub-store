1a6e95e054d7551c565d61288052236c0a89c14c
Nasa-ee Madadam <122335609+Nasaee@users.noreply.github.com>
2023-12-13 00:58:36 +0700

    setup Loading to Home component

--- src/page/Home.page.tsx
+++ src/page/Home.page.tsx
@@ -1,20 +1,27 @@
 import { Footer, Navbar } from '../components';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import { fetchProductsStart } from '../store/products/productsSlice';
 import { useDispatch } from 'react-redux';
 import { Toaster } from 'react-hot-toast';
+import Loading from '../components/Loading';
 
 const Home = () => {
   const dispatch = useDispatch();
+  const navigation = useNavigation();
+  const isLoading = navigation.state === 'loading';
 
   dispatch(fetchProductsStart());
 
   return (
     <>
       <Navbar />
-      <section className='align-element min-h-screen'>
-        <Outlet />
-      </section>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <section className='align-element min-h-screen'>
+          <Outlet />
+        </section>
+      )}
       <Toaster position='top-center' reverseOrder={true} />
       <Footer />
     </>
