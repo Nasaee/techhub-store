64bdc26bf851f92d2ffeabc17c566cf9ccb8a10c
Nasa-ee Madadam <122335609+Nasaee@users.noreply.github.com>
2023-12-15 00:31:01 +0700

    Revert "setup loading page"
    This reverts commit 042532d30e24189470236f57c97f4594addee80c.

--- src/store/root-reducer.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import productsReducer from './products/productsSlice';
-import userReducer from './user/userSlice';
-
-export const rootReducer = combineReducers({
-  products: productsReducer,
-  user: userReducer,
-});
--- src/store/root-saga.ts
+++
@@ -1,7 +0,0 @@
-import { all, call } from 'redux-saga/effects';
-
-import { productsSaga } from './products/products.saga';
-
-export function* rootSaga() {
-  yield all([call(productsSaga)]);
-}
