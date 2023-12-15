76f4f1f7f1ebc921befc003aad92405f52c6b73c
Nasa-ee Madadam <122335609+Nasaee@users.noreply.github.com>
2023-12-15 12:01:02 +0700

    revert to create signup page markup

--- src/page/Home.page.tsx
+++ src/page/Home.page.tsx
@@ -1,22 +1,12 @@
 import { Footer, Navbar } from '../components';
-import { Outlet, useNavigation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { fetchProductsStart } from '../store/products/productsSlice';
 import { useDispatch } from 'react-redux';
 import { Toaster } from 'react-hot-toast';
-<<<<<<< HEAD
 import { useEffect } from 'react';
-=======
->>>>>>> parent of a87d004 (Revert "create signup page markup")
-import Loading from '../components/Loading';
 
 const Home = () => {
   const dispatch = useDispatch();
-<<<<<<< HEAD
-  const isLoading = useNavigation().state === 'loading';
-=======
-  const navigation = useNavigation();
-  const isLoading = navigation.state === 'loading';
->>>>>>> parent of a87d004 (Revert "create signup page markup")
 
   useEffect(() => {
     dispatch(fetchProductsStart());
@@ -25,13 +15,9 @@ const Home = () => {
   return (
     <>
       <Navbar />
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <section className='align-element min-h-screen'>
-          <Outlet />
-        </section>
-      )}
+      <section className='align-element min-h-screen'>
+        <Outlet />
+      </section>
       <Toaster position='top-center' reverseOrder={true} />
       <Footer />
     </>
--- src/page/SingleProduct.page.tsx
+++ src/page/SingleProduct.page.tsx
@@ -1,31 +1,4 @@
 const SingleProduct = () => {
-  const { product }: { product: SingleProductData } = useLoaderData() as {
-    product: SingleProductData;
-  };
-
-  const {
-    name,
-    brand,
-    category,
-    stock,
-    description,
-    colors,
-    price,
-    featured,
-    images,
-    screen_size,
-    cpu,
-    cpu_details,
-    display,
-    memory,
-    os,
-    font_camera,
-    back_camera,
-    battety,
-    weight,
-    warranty,
-  } = product;
-
-  return <div>{product?.name}</div>;
+  return <div>Singleproduct</div>;
 };
 export default SingleProduct;
