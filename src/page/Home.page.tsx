e69281e841f671f441e37b13201f33ee645a0e22
Nasa-ee Madadam <122335609+Nasaee@users.noreply.github.com>
2023-12-15 12:09:39 +0700

    render loading page when products is loading

--- src/page/Home.page.tsx
+++ src/page/Home.page.tsx
@@ -1,12 +1,15 @@
 import { Footer, Navbar } from '../components';
 import { Outlet } from 'react-router-dom';
 import { fetchProductsStart } from '../store/products/productsSlice';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Toaster } from 'react-hot-toast';
 import { useEffect } from 'react';
+import { selectIsProductsLoading } from '../store/products/products.selector';
+import Loading from '../components/Loading';
 
 const Home = () => {
   const dispatch = useDispatch();
+  const isProductsLoading = useSelector(selectIsProductsLoading);
 
   useEffect(() => {
     dispatch(fetchProductsStart());
@@ -15,9 +18,14 @@ const Home = () => {
   return (
     <>
       <Navbar />
-      <section className='align-element min-h-screen'>
-        <Outlet />
-      </section>
+      {isProductsLoading ? (
+        <Loading />
+      ) : (
+        <section className='align-element min-h-screen'>
+          <Outlet />
+        </section>
+      )}
+
       <Toaster position='top-center' reverseOrder={true} />
       <Footer />
     </>
